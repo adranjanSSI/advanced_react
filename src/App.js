23a41e7ed8be57d39bc1d0278db6d2d37374d9e0
RANJAN Aditya <aditya.ranjan@soprateria.com>
2021-08-11 14:20:11 +0530

    Learning UseEffect Basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
+//import UseStateObject from "./tutorial/useStateobject";
+//import UseStateBasics from './tutorial/UseStateBasics';
+//import UseStateArray from "./tutorial/UseStateArray";
 //import UseStateobject from "./tutorial/useStateobject";
-import UseStateBasics from './tutorial/UseStateBasics';
-import UseStateArray from "./tutorial/UseStateArray";
+//import UseStateCounter from "./tutorial/UseStateCounter";
+import UseEffectBasics from "./tutorial/UseEffectBasics";
 function App() {
   return (
     <div className="container"><h2>Advanced React JS</h2>
-    <UseStateArray/>
+    <UseEffectBasics/>
     </div>
     );
   }
