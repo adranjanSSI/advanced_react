e9efb49e97d303b8d8fd3553795614e06bc908f9
RANJAN Aditya <aditya.ranjan@soprateria.com>
2021-08-11 14:19:03 +0530

    Learning UseEffect Basics

---
+++ src/tutorial/UseEffectBasics.js
@@ -0,0 +1,21 @@
+import React,{useState,useEffect} from 'react'
+
+function UseEffectBasics() {
+    const [value,setValue]=useState(0);
+    useEffect(()=>
+    {
+        console.log('call UseEffect');
+        document.title=`New Messages(${value})`;
+    });
+    console.log('render component');
+    return (
+        <>
+            <h1>{value}</h1>
+            <button className='btn' onClick={()=>setValue(value+1)}>
+                Click Me
+            </button>
+            </>
+    )
+}
+
+export default UseEffectBasics
