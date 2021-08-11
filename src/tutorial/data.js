5d4470a60817bc80739183ed247d2b85674ccc78
RANJAN Aditya <aditya.ranjan@soprateria.com>
2021-08-11 10:04:15 +0530

    Array Data for usestatearray

---
+++ src/tutorial/data.js
@@ -0,0 +1,8 @@
+export const data=[
+    {id:1, name:"Bruce Wayne"},
+    {id:2, name:"Barry Allen"},
+    {id:3, name:"Dr. Strange"},
+    {id:4, name:"John Wick"},
+    {id:5, name:"Harry Potter"},
+    {id:6, name:"Reverse Flash"}
+];
