c789811da06d43d87928080f03fee5babf69e450
RANJAN Aditya <aditya.ranjan@soprateria.com>
2021-08-12 11:40:46 +0530

    Using JSON server

--- src/tutorial/LearnJSON.js
+++ src/tutorial/LearnJSON.js
@@ -3,26 +3,32 @@ import axios from 'axios'
 function LearnJSON() {
     const handleClick=(b)=>
     {
-        const promise=axios.post("http://localhost:4000/users",
-        {name:"Bruce Wayne"},{
+        const promise=axios.post("http://localhost:4000/users"//setting the port code
+        ,{name:"Barry Allen"},{
             headers:{
-                ['content-type']:'application/json'}
+                ['content-type']:'application/json'//setting the server type
+            }
             
         })
     
-    promise.then(response=>{console.log(response)
+    promise.then(response=>{console.log(response)//this is used to console log aur response from the server.
     })
     promise.catch(error=>{
-        console.error(error);
+        console.error(error);//this is used to console log aur error in case we get one.
     })
     let c=b;
     console.log(c);
+}
+const getUsers=()=>{
+    axios.get("http://localhost:4000/users").then(users=>{
+        console.log(users);//we are getting the data from our json server and then storing it in users then consoling the array of the objects.
+    })
 }
     return (
         <>
             <h2>Randome Title</h2>
             <button type='button' className='btn' onClick={handleClick}>post users</button>
-           {/*<button type='button' className='btn' onClick={getUsers}>Get users</button>*/}
+           <button type='button' className='btn' onClick={getUsers}>Get users</button>
         </>
     )
 }
